18719bbb54750856e20e221d0e7a90954c37f21e
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2025-06-07 10:58:13 +0900

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#006989',
+        secondary: '#E88D67',
+        accent: '#005C78',
+        background: '#F3F7EC',
+      },
+    },
+  },
+  plugins: [],
+};
