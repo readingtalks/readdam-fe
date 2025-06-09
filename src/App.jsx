0f015ff3c73731b68a4d91cc0826b194ba50c516
Hyunhee Kim <63030323+hyeonhe@users.noreply.github.com>
2025-06-09 19:56:12 +0900

    placeAdd, placeReservationList 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,19 @@ import './index.css';
 import { Routes, Route } from 'react-router-dom';
 import PlaceList from './pages/admin/PlaceList';
 import AdminRoutes from './Routes/AdminRoutes';
+import PlaceAdd from './pages/admin/PlaceAdd';
+import PlaceReservationList from './pages/admin/PlaceReservationList';
 
 function App() {
   return (
     <>
       <Routes>
         <Route element={<AdminRoutes />}>
+          <Route path="/placeList" element={<PlaceList />} />
+          <Route path="/placeAdd" element={<PlaceAdd />} />
           <Route
-            path="/placeList"
-            element={
-              <main className="flex-grow">
-                <PlaceList />
-              </main>
-            }
+            path="/placeReservationList"
+            element={<PlaceReservationList />}
           />
         </Route>
       </Routes>
